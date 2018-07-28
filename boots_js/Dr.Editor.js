8f90e21c659bab1830aa7de70fa67698b238290d
drwebsax <39692268+drwebsax@users.noreply.github.com>
2018-07-28 10:23:39 +0900

    0728

--- boots_js/Dr.Editor.js
+++ boots_js/Dr.Editor.js
@@ -11,53 +11,53 @@
 
 	DrEdit.prototype = {
 		init : function() {
-            DrAJAX = new Dr.Ajax();
+			DrAJAX = new Dr.Ajax();
 			that.preset.load_json(); //초기 로드해야함
-            //$('textarea').autoGrowTextArea();
-        }, // init end
-        textSave: function() {
-	       Dr.Framework.textSave();
-        },
-	    preset :{
+			//$('textarea').autoGrowTextArea();
+		}, // init end
+		textSave: function() {
+	        	Dr.Framework.textSave();
+		},
+		preset :{
 			load_json :function(){
-			var jsondata =	DrAJAX.LoadJsonData('./ajax/init.json'); //'./ajax/osc.xml'
-			//console.log(jsondata.editor);
+				var jsondata =	DrAJAX.LoadJsonData('./ajax/init.json'); //'./ajax/osc.xml'
+				//console.log(jsondata.editor);
 			}
-	    },
+		},
 		ajaxSet :{
-	        callXmldata : function(url){   //
+			callXmldata : function(url){   //
 				var xml =	DrAJAX.ParseXml(url); //'./ajax/osc.xml'
 				var $xmldata = $(xml).find("audio").find("title").text();
 				//console.log($xmldata);
-	            // var user_image_list = (xmldata == null) ? "sssss": "fdsadaf";
-	            //   console.log(user_image_list);
-	        }
-		 }//manager mmode 끝
-	}; // DrEdit.prototype 끝
+				// var user_image_list = (xmldata == null) ? "sssss": "fdsadaf";
+				//   console.log(user_image_list);
+			}
+			}//manager mmode 끝
+		}; // DrEdit.prototype 끝
 
-    $(".call_ajax").click(function(){ // load ajax xml
-		var synthesisName =$(this).attr('id');
-		var urlData ='./ajax/'+synthesisName+'.xml'
-	    //console.log(urlData)
-        that.ajaxSet.callXmldata(urlData); //
-     });
+		$(".call_ajax").click(function(){ // load ajax xml
+			var synthesisName =$(this).attr('id');
+			var urlData ='./ajax/'+synthesisName+'.xml'
+			//console.log(urlData)
+			that.ajaxSet.callXmldata(urlData); //
+		});
 
-     $('#text_play').click(function(){
+		$('#text_play').click(function(){
 
-     	$('#real').html($('#txt').val());
-     });
+			$('#real').html($('#txt').val());
+		});
 
-     $('#text_one').click(function(){
-         $("#txt").val("<script></script>");
-     });
+		$('#text_one').click(function(){
+			$("#txt").val("<script></script>");
+		});
 
-	 $('#text_save').click(function(){
- 		 that.textSave();
- 	 });
+		$('#text_save').click(function(){
+			that.textSave();
+		});
 
-     // Dr.Framework.Ready("gds");
-	 Constructor.data = DrEdit;
-	 DrEdit();
+		// Dr.Framework.Ready("gds");
+		Constructor.data = DrEdit;
+		DrEdit();
 
 })(window, DrEditor);
 
